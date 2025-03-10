9873497c4df793a793fc824f8709e4aef74a838e
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-03-10 19:03:02 -0400

    Worked on Beta Probability function

--- HW07.R
+++ HW07.R
@@ -1,7 +1,12 @@
 #where do we use size?
+#specify x limits
 
 #Question 1 - pois.prob function
 pois.prob <- function(x, lambda, type="<="){
+  #check if x is valid for Poisson distribution
+  if (x<0 | x != floor(x)){
+    return("Invalid argument for 'x'")
+  }
   if (type == "="){ #compute P(X=x)
     return(dpois(x, lambda))
   }else if(type == "!="){  #compute P(X!=x)
@@ -14,6 +19,29 @@ pois.prob <- function(x, lambda, type="<="){
     return(1 - ppois(x,lambda))
   }else if (type == ">="){  #compute P(X>=x)
     return(1 - ppois(x -1,lambda))
+  }else{
+    return("Invalid argument for 'type'")
+  }
+}
+
+#Question 2 - beta.prob function
+beta.prob <- function(x, alpha, beta, type="<="){
+  #check if x is valid for Beta distribution
+  if (x<0 | x>1){
+    return("Invalid argument for 'x'")
+  }
+  if (type == "="){ #compute P(X=x)
+    return(dbeta(x, alpha, beta))
+  }else if(type == "!="){  #compute P(X!=x)
+    return(1 - dbeta(x, alpha, beta))
+  }else if(type == "<"){  #compute P(X<x)
+    return(pbeta(x, alpha, beta))
+  }else if(type == "<="){ #compute P(X<=x)
+    return(pbeta(x, alpha, beta))
+  }else if(type == ">"){  #compute P(X>x)
+    return(1 - pbeta(x, alpha, beta))
+  }else if (type == ">="){  #compute P(X>=x)
+    return(1 - pbeta(x, alpha, beta))
   }else{
     return("Invalid argument for 'type'")
   }
